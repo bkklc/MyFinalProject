e8fa0affa44804a0cf5ce82a5fcc2dd81ed43e06
bkklc <abkklc@gmail.com>
2023-11-22 15:01:57 +0300

    Core layer is added Code refactoring DTos layer is added

--- Business/Abstracts/IProductService.cs
+++ Business/Abstracts/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concretes;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Business.Abstracts
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitprice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
     }
 }
  
--- DataAccess/Abstracts/IOrderDal.cs
+++ DataAccess/Abstracts/IOrderDal.cs
@@ -1,4 +1,6 @@
 ﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Concretes.EntityFramework;
 using Entities.Concretes;
 using System;
 using System.Collections.Generic;
@@ -8,8 +10,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstracts
 {
-    public interface ICustomerDal : IEntityRepository<Customer>
+    public interface IOrderDal : IEntityRepository<Order>
     {
-
     }
 }
