6a6664e5159e9d3ecd954fb4b1157061a691f115
bkklc <abkklc@gmail.com>
2023-12-04 16:18:55 +0300

    Web API has been added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using Business.Abstracts;
+using Business.Concretes;
+using Core.Utilities.Results;
+using DataAccess.Concretes.EntityFramework;
+using Entities.Concretes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // ATTRIBUTE - Bir class ile ilgi bilgi verme, onu imzalama yöntemi 
+    public class ProductsController : ControllerBase
+    {
+        // Loosely Coupled
+        // naming convention
+        // IoC Container - Inverison Of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            // Dependency Chain
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+    }
+}
